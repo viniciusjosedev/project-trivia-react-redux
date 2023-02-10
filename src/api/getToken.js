8c5e981eba909186a0d798f5eb698e3fffa8d9ac
Vinicius José <viniciusjosedev@gmail.com>
2023-02-09 22:45:58 -0300

    Conflitos resolvidos

---
+++ src/api/getToken.js
@@ -0,0 +1,7 @@
+const getToken = async () => {
+  const requisicao = await (await fetch('https://opentdb.com/api_token.php?command=request')).json();
+  // const data = await requisicao.json();
+  localStorage.setItem('token', requisicao.token);
+};
+
+export default getToken;
