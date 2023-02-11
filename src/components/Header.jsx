0a7a0012c605c5732cfc6e6c140fb9d95b1584fa
Heitor Soares <heytorpg7@gmail.com>
2023-02-10 23:10:44 -0300

    moved getGravatarImg() to helper folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import gravatarImg from '../api/getGravatarImg';
+import gravatarImg from '../helper/getGravatarImg';
 
 class Header extends Component {
   state = {
