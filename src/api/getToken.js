295624131e946ca400004f4990cfd4ad29d9a5b8
Vinicius José <viniciusjosedev@gmail.com>
2023-02-16 13:23:50 -0300

    lint fix

--- src/api/getToken.js
+++ src/api/getToken.js
@@ -7,12 +7,16 @@ const getToken = async (history) => {
 
 export const getQuestions = async (token) => {
   const getStorage = JSON.parse(localStorage.getItem('settings'));
-  const category = getStorage.category > 0 ? `category=${getStorage.category}` : '';
-  const difficulty = getStorage.difficulty
-    !== 'any' ? `difficulty=${getStorage.difficulty}` : '';
-  const type = getStorage.type
-    !== 'any' ? `type=${getStorage.type}` : '';
-  const requisicao = await (await fetch(`https://opentdb.com/api.php?amount=5&token=${token}&${category}&${difficulty}&${type}`)).json();
+  if (getStorage !== null) {
+    const category = getStorage.category > 0 ? `category=${getStorage.category}` : null;
+    const difficulty = getStorage.difficulty
+    !== 'any' ? `difficulty=${getStorage.difficulty}` : null;
+    const type = getStorage.type
+    !== 'any' ? `type=${getStorage.type}` : null;
+    const requisicao = await (await fetch(`https://opentdb.com/api.php?amount=5&token=${token}&${category}&${difficulty}&${type}`)).json();
+    return requisicao;
+  }
+  const requisicao = await (await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`)).json();
   return requisicao;
 };
 
