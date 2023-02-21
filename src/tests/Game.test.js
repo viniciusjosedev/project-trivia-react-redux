8a614a9a5177bc5fae71f84246e3f40163e4b9e9
Vinicius José <viniciusjosedev@gmail.com>
2023-02-21 18:19:26 -0300

    Requisito 21 feito

---
+++ src/tests/Game.test.js
@@ -0,0 +1,153 @@
+import React from "react";
+import { screen, waitFor } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
+import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+import App from '../App';
+import Game from "../pages/Game";
+import { fetchDataMockado } from './mocks/data';
+
+describe('All tests from screen Game', () => {
+	it('Testa se a pagina do game esta sendo chamada', async () => {
+		const { history } = renderWithRouterAndRedux(<App />)
+
+		const inputName = screen.getByTestId('input-player-name');
+		const inputEmail = screen.getByTestId('input-gravatar-email');
+		const buttonPlay = screen.getByTestId('btn-play');
+
+
+		userEvent.type(inputName, 'Vinicius');
+		userEvent.type(inputEmail, 'a@gmail.com')
+		userEvent.click(buttonPlay);
+
+		await waitFor(() => {
+			expect(screen.getByAltText(/estrela dos pontos/i)).toBeInTheDocument();
+			expect(history.location.pathname).toEqual('/game')
+		}, { timeout: 5000 })
+
+	}, 30000)
+
+	it('Clica no botão /próxima/', async () => {
+		const { history } = renderWithRouterAndRedux(<App />)
+
+		const inputName = screen.getByTestId('input-player-name');
+		const inputEmail = screen.getByTestId('input-gravatar-email');
+		const buttonPlay = screen.getByTestId('btn-play');
+
+		userEvent.type(inputName, 'Vinicius');
+		userEvent.type(inputEmail, 'a@gmail.com')
+		userEvent.click(buttonPlay);
+		
+		let buttonNext = await screen.findByText(/próxima/i)
+
+		// await waitFor(() => {
+		// 	buttonNext = screen.getByText(/próxima/i)
+    //   expect(screen.getByTestId('text-timer').textContent).toEqual('Tempo: 21s')
+		// }, { timeout: 10000 })
+
+		userEvent.click(buttonNext);
+		userEvent.click(buttonNext);
+		userEvent.click(buttonNext);
+		userEvent.click(buttonNext);
+		userEvent.click(buttonNext);
+
+		await waitFor(() => {
+			expect(history.location.pathname).toEqual('/feedbacks')
+		}, { timeout: 5000 })
+
+	}, 30000)
+
+	it('testa e está indo para a página de Game', async () => {
+		const { history } = renderWithRouterAndRedux(<App />)
+
+		const inputName = screen.getByTestId('input-player-name');
+		const inputEmail = screen.getByTestId('input-gravatar-email');
+		const buttonPlay = screen.getByTestId('btn-play');
+
+		userEvent.type(inputName, 'Vinicius');
+		userEvent.type(inputEmail, 'a@gmail.com')
+		userEvent.click(buttonPlay);
+
+		await waitFor(() => {
+			expect(history.location.pathname).toEqual('/game')
+		}, { timeout: 5000 })
+	}, 30000)
+
+	it('Passa por todos os clicks de next', async () => {
+		renderWithRouterAndRedux(<App />)
+
+
+		const inputName = screen.getByTestId('input-player-name');
+		const inputEmail = screen.getByTestId('input-gravatar-email');
+		const buttonPlay = screen.getByTestId('btn-play');
+
+		userEvent.type(inputName, 'Vinicius');
+		userEvent.type(inputEmail, 'a@gmail.com')
+		userEvent.click(buttonPlay);
+
+		let responseCorrect;
+		let buttonNext;
+		await waitFor(() => {
+			responseCorrect = screen.getByTestId('correct-answer');
+			buttonNext = screen.getByText(/próxima/i)
+		}, { timeout: 5000 })
+
+		expect(responseCorrect).toBeInTheDocument();
+		userEvent.click(responseCorrect);
+		userEvent.click(buttonNext);
+
+		userEvent.click(responseCorrect);
+		userEvent.click(buttonNext);
+		
+		userEvent.click(responseCorrect);
+		userEvent.click(buttonNext);
+
+		userEvent.click(responseCorrect);
+		userEvent.click(buttonNext);
+
+		userEvent.click(responseCorrect);
+		userEvent.click(buttonNext);
+	}, 30000)
+
+	it('', async () => {
+		renderWithRouterAndRedux(<App />)
+
+		const inputName = screen.getByTestId('input-player-name');
+		const inputEmail = screen.getByTestId('input-gravatar-email');
+		const buttonPlay = screen.getByTestId('btn-play');
+
+		userEvent.type(inputName, 'Vinicius');
+		userEvent.type(inputEmail, 'a@gmail.com')
+		userEvent.click(buttonPlay);
+
+		await waitFor(() => {
+			expect(screen.getByTestId('text-timer').textContent).toContain('Tempo: 1s')
+		}, { timeout: 35000 })
+
+	}, 50000)
+	
+	it('Testa se quando o /responde_code/ é 0 o usuario é redirecionado para a /', async () => {
+		global.fetch = jest.fn().mockResolvedValue({
+			json: jest.fn().mockResolvedValue(fetchDataMockado),
+		})
+
+		const { history } = renderWithRouterAndRedux(<App />)
+
+
+		const inputName = screen.getByTestId('input-player-name');
+		const inputEmail = screen.getByTestId('input-gravatar-email');
+		const buttonPlay = screen.getByTestId('btn-play');
+
+		userEvent.type(inputName, 'Vinicius');
+		userEvent.type(inputEmail, 'a@gmail.com')
+		userEvent.click(buttonPlay);
+
+		await waitFor(() => {
+			expect(history.location.pathname).toEqual('/game')
+		}, { timeout: 5000 })
+
+		await waitFor(() => {
+			expect(history.location.pathname).toEqual('/')
+		}, { timeout: 5000})
+		jest.clearAllMocks();
+	}, 30000)
+})	
---
+++ src/tests/mocks/data.js
@@ -0,0 +1,65 @@
+export const fetchDataMockado = {
+	"response_code": 3,
+	"results": [
+		{
+			"category": "Sports",
+			"type": "multiple",
+			"difficulty": "easy",
+			"question": "In golf, what name is given to a hole score of two under par?",
+			"correct_answer": "Eagle",
+			"incorrect_answers": [
+				"Birdie",
+				"Bogey",
+				"Albatross"
+			]
+		},
+		{
+			"category": "General Knowledge",
+			"type": "multiple",
+			"difficulty": "medium",
+			"question": "What is the unit of currency in Laos?",
+			"correct_answer": "Kip",
+			"incorrect_answers": [
+				"Ruble",
+				"Konra",
+				"Dollar"
+			]
+		},
+		{
+			"category": "Entertainment: Japanese Anime & Manga",
+			"type": "multiple",
+			"difficulty": "medium",
+			"question": "In the &quot;Re:Zero&quot; manga series, which of the following Sin Archbishops eats Rem&#039;s existence?",
+			"correct_answer": "Ley Batenkaitos",
+			"incorrect_answers": [
+				"Roy Alphard",
+				"Petelgeuse Romanee-Conti",
+				"Louis Arneb"
+			]
+		},
+		{
+			"category": "Animals",
+			"type": "multiple",
+			"difficulty": "medium",
+			"question": "&quot;Decapods&quot; are an order of ten-footed crustaceans.  Which of these are NOT decapods?",
+			"correct_answer": "Krill",
+			"incorrect_answers": [
+				"Lobsters",
+				"Shrimp",
+				"Crabs"
+			]
+		},
+		{
+			"category": "General Knowledge",
+			"type": "multiple",
+			"difficulty": "hard",
+			"question": "What is the most commonly used noun in the English language?",
+			"correct_answer": "Time",
+			"incorrect_answers": [
+				"Home",
+				"Water",
+				"Man"
+			]
+		}
+	]
+}
